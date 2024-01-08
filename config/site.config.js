73e12422f8e602c280ede10a2c93661fa337beab
imagine rakan <69947557+imagine-rakan@users.noreply.github.com>
2024-01-08 07:15:36 +0300

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -42,8 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  "protectedRoutes": [
-  "/upload/vid"],
+  protectedRoutes: ['/🌞 Private folder/upload/vid'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
